7e4027c20b1ef3403c6445f3b1644709165b75b8
Rio Kasahara <you@example.com>
2021-04-23 23:41:31 +0900

    jstest

---
+++ js/jspractice.js
@@ -0,0 +1,35 @@
+//document.write("Hello World!");
+//alert("Hello World!");
+//console.log("Hello World!"+"七海さん");
+//console.log("1"+"1");
+//console.log(1+1);
+
+//console.log(1+1)
+//console.log(1-1)
+//console.log(2*2)
+//console.log(10/3)
+//console.log(10%3)
+
+//let x = 1;
+//let y = 1;
+//x++;
+//console.log(x);
+//y--;
+//console.log(y);
+
+//let x = 7;
+//let y = x++;
+//let x2 = 7;
+//let y2 = ++x2;
+
+//console.log(x);
+//console.log(y);
+//console.log(x2);
+//console.log(y2);
+
+//let age = 27;
+//const name = "七海建人";
+
+//age = age+1;
+
+//console.log(`${name}さんの年齢は${age}です`);
